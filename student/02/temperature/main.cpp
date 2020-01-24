aa7f8608ae743536b47336a8c7806a0682830d3e
The Anh <anh.t.nguyen@tuni.fi>
2020-01-25 00:42:15 +0200

    hi

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -8,7 +8,7 @@ int main()
     int temp;
 
     cin >> temp;
-    cout << temp << ' degrees Celsius is ' << 1.8* temp +32 << ' degrees Fahrenheit!' <<endl;
-    cout << temp << ' degrees Fahrenheit is ' << (temp - 32)/1.8 << ' degree Celsius!' << endl;
+    cout << temp << " degrees Celsius is " << 1.8* temp +32 << " degrees Fahrenheit!" <<endl;
+    cout << temp << " degrees Fahrenheit is " << (temp - 32)/1.8 << " degree Celsius!" << endl;
     return 0;
 }
