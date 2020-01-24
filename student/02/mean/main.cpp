b52c0989ef9d01056bb251b9543383dc5985a7b8
The Anh <anh.t.nguyen@tuni.fi>
2020-01-25 01:37:27 +0200

    hi

--- student/02/mean/main.cpp
+++ student/02/mean/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+using namespace std;
+int mean(int number_of_components)
+{
+        int i = 1, sum, x;
+        if (number_of_components >=1) {
+                while (i <= number_of_components) {
+                        cout << "Input " << i << ". number: ";
+                        cin >> x;
+                        sum += x;
+                        i++;
+                }
+                cout << "Mean value of the given numbers is "<< (sum/number_of_components);
+                return 0;
 
-// Write here a function counting the mean value
+        }
+        else {
+                cout << "Cannot count mean value from 0 numbers";
+        }
+}
 
 int main()
 {
-    std::cout << "From how many integer numbers you want to count the mean value? ";
+    int num;
+        cout << "From how many integer numbers you want to count the mean value? ";
+    cin >> num;
+    mean(num);
+    return 0;
 }
