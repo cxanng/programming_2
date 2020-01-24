37a208a7421195acacd3d5b92d3fc189dc357ed5
The Anh <anh.t.nguyen@tuni.fi>
2020-01-25 01:10:43 +0200

    hello

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,21 +3,22 @@
 using namespace std;
 int main()
 {
-    cout << "How many numbers would you like to have? ";
     int count = 1, num;
+        cout << "How many numbers would you like to have? ";
+
     cin >> num;
     while (count <= num) {
         if (count % 3 == 0 && count % 7 !=0) {
-                cout << "zip";
+                cout << "zip" << endl;
                 }
-                else if (count % 3 != 0 &&count % 7 ==0) {
-                        cout << "boing";
+                else if (count % 3 != 0 && count % 7 ==0) {
+                        cout << "boing" << endl;
                 }
                 else if (count % 3 == 0 && count % 7 ==0) {
-                        cout << "zip boing";
+                        cout << "zip boing" << endl;
                 }
                 else {
-                        cout << count;
+                        cout << count << endl;
                 }
                 count++;
 
