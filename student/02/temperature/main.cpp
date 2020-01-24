9a43870f9f4457f77b6bbb06abb18f7d770447e4
The Anh <anh.t.nguyen@tuni.fi>
2020-01-25 00:40:10 +0200

    hi

---
+++ student/02/temperature/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a temperature: ";
+    int temp;
+
+    cin >> temp;
+    cout << temp << ' degrees Celsius is ' << 1.8* temp +32 << ' degrees Fahrenheit!' <<endl;
+    cout << temp << ' degrees Fahrenheit is ' << (temp - 32)/1.8 << ' degree Celsius!' << endl;
+    return 0;
+}
