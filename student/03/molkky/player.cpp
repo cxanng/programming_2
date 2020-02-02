56b8b78a4944f3c923c0c038f4052a12cf49e373
The Anh <anh.t.nguyen@tuni.fi>
2020-02-02 22:56:22 +0200

    test3

--- student/03/molkky/player.cpp
+++ student/03/molkky/player.cpp
@@ -7,17 +7,17 @@ using namespace std;
 Player::Player(string name): name_(name)
 {
 
-}
+};
 
 void Player::add_points(int number) {
         points_ += number;
         if (points_ > 50) {
             points_ = 25;
         }
-    }
+    };
 string Player::get_name() const {
         return name_;
-    }
+    };
 bool Player::has_won() {
         if (points_ == 50) {
             return true;
@@ -25,7 +25,7 @@ bool Player::has_won() {
         else {
             return false;
         }
-    }
+    };
 int Player::get_points(){
         return points_;
-    }
+    };
