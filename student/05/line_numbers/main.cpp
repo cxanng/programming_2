b605fd8666e8d7baa06719639947d0d041103f1e
The Anh <anh.t.nguyen@tuni.fi>
2020-02-15 00:22:12 +0200

    test5

--- student/05/line_numbers/main.cpp
+++ student/05/line_numbers/main.cpp
@@ -10,24 +10,17 @@ int main()
     std::cout << "Output file: ";
     std::cin >> outfile_name;
     std::ifstream instream(infile_name);
-//    instream.open(infile_name);
-    std::ofstream outstream(outfile_name);
-//    outstream.open(outfile_name);
-    if (not instream) {
+    if (! instream) {
         std::cout << "Error! The file " << infile_name << " can not be opened." << std::endl;
     }
     else {
-        unsigned int i = 1;
-        while (not instream.eof()) {
-            std::string line = "";
-            std::getline(instream, line);
-            if (line != "") {
-                outstream << i << " " << line << "\n";
-                i++;
-            }
+        std::ofstream outstream(outfile_name);
+        unsigned int i = 0;
+        std::string line = "";
+        while (std::getline(instream, line)) {
+            ++i;
+            outstream << i << " " << line << "\n";
         }
-//        instream.close();
-//        outstream.close();
     }
     return 0;
 }
