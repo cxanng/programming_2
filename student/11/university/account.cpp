68ce703a0d107af67a89e01b709f8f30fb2a5161
The Anh <anh.t.nguyen@tuni.fi>
2020-04-18 13:31:57 +0300

    Add some features in class Account such as current instances, completed courses and credtis as well as functions relating to these features

--- student/11/university/account.cpp
+++ student/11/university/account.cpp
@@ -8,14 +8,17 @@ Account::Account(std::string full_name, std::string email, int account_number):
     last_name_(""),
     first_name_(""),
     email_(email),
-    account_number_(account_number)
+    account_number_(account_number),
+    current_({}),
+    completed_({}),
+    credits_(0)
 {
     std::vector<std::string> name = utils::split(full_name_, ' ');
     last_name_ = name.back();
     first_name_ = name.front();
 }
 
-
+// Prints basic informations of the account user
 void Account::print() const
 {
     std::cout << account_number_ << ": "
@@ -24,7 +27,51 @@ void Account::print() const
               << email_ << std::endl;
 }
 
+// Returns email
 std::string Account::get_email()
 {
     return email_;
+}
+
+// Adds instance to current instance list
+bool Account::add_instance(Instance *ins) {
+    for (auto *instance : current_) {
+        if (instance == ins) {
+            std::cout << ALREADY_REGISTERED << std::endl;
+            return false;
+        }
+    }
+    current_.push_back(ins);
+    return true;
+}
+
+// Completes course
+bool Account::complete_course(Instance *ins) {
+    std::vector<Instance*>::iterator iter = current_.begin();
+    for (iter = current_.begin() ; iter != current_.end(); iter++) {
+        if (*iter == ins) {
+            completed_.push_back(ins->get_course());
+            current_.erase(iter);
+            credits_ += ins->get_course()->get_credits();
+            std::cout << COMPLETED << std::endl;
+            return true;
+        }
+    }
+    std::cout << NO_SIGNUPS << std::endl;
+    return false;
+}
+
+// Returns current
+std::vector<Instance*> Account::get_currents() {
+    return current_;
+}
+
+// Returns completed courses
+std::vector<Course*> Account::get_completed() {
+    return completed_;
+}
+
+// Returns credits
+unsigned int Account::get_credit() {
+    return credits_;
 }
--- student/11/university/account.hh
+++ student/11/university/account.hh
@@ -43,14 +43,55 @@ public:
      */
     std::string get_email();
 
+    /**
+     * @brief add_instance
+     * @param ins
+     * This function checks if the instance has already been registered
+     * and adds the instance to current list of the account
+     */
+    bool add_instance(Instance *ins);
+
+    /**
+     * @brief complete_course
+     * @param ins
+     * Adds the course to completed list, removes the instance from
+     * the current list and add credits to the number of credits of
+     * the student.
+     */
+    bool complete_course(Instance *ins);
+
+    /**
+     * @brief get_currents
+     * Returns current instances
+     */
+    std::vector<Instance*> get_currents();
+
+    /**
+     * @brief get_completed
+     * Returns completed courses
+     */
+    std::vector<Course*> get_completed();
+    /**
+     * @brief get_credit
+     * Returns number of credits completed
+     */
+    unsigned int get_credit();
+
+
 private:
+    // Basic informations of the account user
     std::string full_name_;
     std::string last_name_;
     std::string first_name_;
     std::string email_;
     const int account_number_;
+
+    // List contains current instances and completed courses
     std::vector<Instance*> current_;
     std::vector<Course*> completed_;
+
+    // Number of credits earned
+    unsigned int credits_;
 };
 
 #endif // ACCOUNT_HH
